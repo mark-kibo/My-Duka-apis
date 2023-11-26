22a203bfd2092ea61fad2686140be660caf68243
Masibo <victor.wafula@student.moringaschool.com>
2023-11-26 15:08:04 +0300

    corrected the sign up api

--- api/sign-up/app.py
+++ api/sign-up/app.py
@@ -1,46 +1,56 @@
-# Import necessary modules from Flask and Flask extensions
-from flask import Flask
-from flask_restx import Api, Resource, reqparse
+from flask_restx import Namespace, Resource, fields, reqparse
+from itsdangerous import URLSafeTimedSerializer
+from flask_mail import Message
+from werkzeug.exceptions import BadRequest
+from flask import Flask, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from werkzeug.exceptions import BadRequest
+from utils import mail
 from models.users import Users
 
-
-# Create a Flask web application instance
 app = Flask(__name__)
-
-# Configure the SQLAlchemy extension with the SQLite database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['MAIL_DEFAULT_SENDER'] = 'your_email@example.com'
+app.config['MAIL_SERVER'] = 'smtp.example.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'your_username'
+app.config['MAIL_PASSWORD'] = 'your_password'
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['TOKEN_EXPIRATION'] = 600
 
-# Create an SQLAlchemy database instance and associate it with the Flask app
 db = SQLAlchemy(app)
-
 bcrypt = Bcrypt()
-api = Api()
 
-# Create a Flask-RESTx API instance, providing version and description
-api = Api(app, version='1.0', title='Your API', description='User Signup API')
+signup_ns = Namespace('signup', description='User Signup API')
 
-# Request parser for the signup endpoint, defining expected parameters
-signup_parser = reqparse.RequestParser()
-signup_parser.add_argument('username', type=str, required=True, help='Username is required')
-signup_parser.add_argument('password', type=str, required=True, help='Password is required')
-signup_parser.add_argument('email', type=str, required=True, help='Email is required')
-signup_parser.add_argument('full_name', type=str, required=True, help='Full name is required')
-signup_parser.add_argument('role', type=str, required=True, help='Role is required')
-signup_parser.add_argument('store_id', type=int, required=True, help='Store ID is required')
+signup_model = signup_ns.model('Signup', {
+    'username': fields.String(required=True, description='Username is required'),
+    'password': fields.String(required=True, description='Password is required'),
+    'email': fields.String(required=True, description='Email is required'),
+    'full_name': fields.String(required=True, description='Full name is required'),
+    'role': fields.String(required=True, description='Role is required'),
+    'store_id': fields.Integer(required=True, description='Store ID is required')
+})
 
-# Define a resource for the signup endpoint using Flask-RESTx
-@api.route('/signup')
+serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
+# Define the Flask-RESTx model for email
+email_model = signup_ns.model('Email', {
+    'link': fields.String
+})
+
+@signup_ns.route('/signup')
 class SignupResource(Resource):
-    @api.expect(signup_parser)
+    @signup_ns.expect(signup_model)
     def post(self):
         try:
-            # Parse the request arguments
-            args = signup_parser.parse_args()
+            args = signup_ns.payload
 
-            # Check if the username or email already exists
+            if not is_superuser():
+                raise BadRequest('Only the superuser can initiate admin registration')
+
             existing_user = Users.query.filter_by(username=args['username']).first()
             if existing_user:
                 raise BadRequest('Username already exists')
@@ -49,27 +59,61 @@ class SignupResource(Resource):
             if existing_email:
                 raise BadRequest('Email already exists')
 
-            # Create a new Users instance with the parsed arguments
-            new_user = Users(
-                username=args['username'],
-                email=args['email'],
-                full_name=args['full_name'],
-                role=args['role'],
-                store_id=args['store_id']
-            )
+            token = serializer.dumps(args['email'], salt='email-confirm')
 
-            # Use the set_password method to hash the password
-            new_user.set_password(args['password'])
+            msg = Message("Confirm Your Registration",
+                          sender=app.config['MAIL_DEFAULT_SENDER'],
+                          recipients=[args['email']])
 
-            # Save the new user to the database
-            new_user.save()
-            return {'message': 'User created successfully'}, 201
+            confirmation_link = url_for('confirm_registration', token=token, _external=True)
+
+            msg.body = f"Click the following link to confirm your registration: {confirmation_link}"
+
+            mail.send(msg)
+
+            return {'message': 'Confirmation email sent. Check your email to complete registration.'}, 200
 
         except BadRequest as e:
             return {'error': str(e)}, 400
         except Exception as e:
             return {'error': 'An unexpected error occurred'}, 500
 
-# Run the Flask application if executed as the main script
+@signup_ns.route('/confirm_registration/<token>')
+class ConfirmRegistration(Resource):
+    def get(self, token):
+        try:
+            args = signup_ns.payload  # Pass args as a parameter or obtain them from the request
+            
+            email = serializer.loads(token, salt='email-confirm', max_age=app.config['TOKEN_EXPIRATION'])
+            
+            if email:
+                # Email confirmed, proceed with admin registration
+                # Assuming you have an Admins model
+                from models.admins import Admins
+                
+                # Create a new Admins instance with the parsed arguments
+                new_admin = Admins(
+                    username=args['username'],
+                    email=args['email'],
+                    full_name=args['full_name'],
+                    role=args['role'],
+                    store_id=args['store_id']
+                )
+                
+                # Use the set_password method to hash the password
+                new_admin.set_password(args['password'])
+                
+                # Save the new admin to the database
+                new_admin.save()
+                
+                return {'message': 'Admin registered successfully'}, 200
+            else:
+                return {'error': 'Token expired or invalid'}, 400
+        except Exception as e:
+            return {'error': 'An unexpected error occurred'}, 500
+
+# Include the ConfirmTokenizeEmail resource in the same namespace
+signup_ns.add_resource(ConfirmTokenizeEmail, '/confirm_tokenized_email/<token>')
+
 if __name__ == '__main__':
     app.run(debug=True)
