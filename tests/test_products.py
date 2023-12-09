fd8c3b0902d1e11e2a268512dc22cb69a0f1c6fd
labankibet <laban.kibet@moringaschool.com>
2023-12-09 13:42:52 +0300

    mocking products query

--- tests/test_products.py
+++ tests/test_products.py
@@ -11,4 +11,9 @@ def test_get_products(product_name, description, expected_response):
     """
     Test that `get_products` returns a list of all products.
     """
-    
+    with pytest.mock.patch.object(Products, "query"):
+    Products.query.all.return_value = [
+        Products(product_id=1, product_name=product_name, description=description),
+        Products(product_id=2, product_name="Another Product", description="Another description"),
+    ]
+
