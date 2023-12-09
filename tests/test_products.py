0589052a7bbfe4026c8aef36e1c63a36ce0de757
labankibet <laban.kibet@moringaschool.com>
2023-12-09 17:41:58 +0300

    test case

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,84 +1,98 @@
 import pytest
-from .products import ProductsResource, product_parser, product_model
+from ..api.products.views import ProductsResource, product_parser, product_model
+from ..api.models.products import Products
 
-
-@pytest.mark.parametrize("product_name, description, expected_response", [
-    ("Test Product 1", "This is a test product.", 200),
-    ("Test Product 2", "Another test product.", 200),
-])
-def test_get_products(product_name, description, expected_response):
-    """
-    Test that `get_products` returns a list of all products.
-    """
-    with pytest.mock.patch.object(Products, "query"):
-        Products.query.all.return_value = [
-            Products(product_id=1, product_name=product_name, description=description),
-            Products(product_id=2, product_name="Another Product", description="Another description"),
-        ]
-
-        resource = ProductsResource()
-        response = resource.get()
-
-        assert response.status_code == expected_response
-        assert len(response.json["products"]) == 2
-        assert response.json["products"][0]["product_name"] == product_name
-        assert response.json["products"][0]["description"] == description
-
-
-def test_get_product_valid_id():
-    """
-    Test that `get_product` returns the correct product details for a valid ID.
-    """
-    with pytest.mock.patch.object(Products, "query"):
-        Products.query.filter_by.return_value.first.return_value = Products(
-            product_id=1, product_name="Test Product", description="This is a test product."
-        )
-
-        resource = ProductsResource()
-        response = resource.get(1)
-
-        assert response.status_code == 200
-        assert response.json["product_id"] == 1
-        assert response.json["product_name"] == "Test Product"
-        assert response.json["description"] == "This is a test product."
-
-
-def test_get_product_invalid_id():
-    """
-    Test that `get_product` returns a 404 error for an invalid ID.
-    """
-    with pytest.mock.patch.object(Products, "query"):
-        Products.query.filter_by.return_value.first.return_value = None
-
-        resource = ProductsResource()
-        response = resource.get(999)
-
-        assert response.status_code == 404
-        assert response.json["message"] == "Product not found"
-
-
+# Test product creation
 def test_create_product():
     """
     Test that `create_product` creates a new product and returns it successfully.
     """
-    with pytest.mock.patch.object(Products, "save"):
-        resource = ProductsResource()
-        response = resource.post(
-            json={
-                "product_name": "New Product",
-                "description": "This is a new product.",
-                "category": "Foodstuff",
-                "brand": "Avena",
-                "quantity": 10,
-                "buying_price": 1200,
-                "selling_price": 1299,
-                "payment_status": "paid",
-                "image_url": "https://naivas.online/103626-medium_default/avena-vegetable-oil-5l.jpg",
-                "store_id": 1,
-                "supplier_id": 2,
-            }
-        )
 
-        assert response.status_code == 201
-        assert response.json["product_name"] == "New Product"
-        assert response.json["description"] == "This is a new product."
+    resource = ProductsResource()
+    new_product = {
+        "product_name": "Test Product",
+        "description": "This is a test product",
+        "category": "Electronics",
+        "brand": "Von",
+        "quantity": 10,
+        "buying_price": 8000,
+        "selling_price": 9000,
+        "payment_status": "Paid",
+        "image_url": "https://naivas.online/101019-medium_default/von-subwoofer-21ch-ves1602fs.jpg",
+        "store_id": 1,
+        "supplier_id": 1,
+    }
+
+    response = resource.post(json=new_product)
+    data = response.json
+
+    assert response.status_code == 201
+    assert data["product_name"] == "Test Product"
+    assert data["description"] == "This is a test product"
+    assert data["category"] == "Foodstuff"
+    assert data["brand"] == "Avena"
+    assert data["quantity"] == 10
+    assert data["buying_price"] == 1200
+    assert data["selling_price"] == 1299
+    assert data["payment_status"] == "Paid"
+    assert data["image_url"] == "https://naivas.online/103626-medium_default/avena-vegetable-oil-5l.jpg"
+    assert data["store_id"] == 1
+    assert data["supplier_id"] == 1
+
+# Test getting a list of products
+def test_get_products():
+    """
+    Test that `get_products` returns a list of all products.
+    """
+
+    resource = ProductsResource()
+    response = resource.get()
+
+    assert response.status_code == 200
+    assert len(response.json["products"]) >= 1
+
+# Test getting a specific product
+def test_get_product():
+    """
+    Test that `get_product` returns the correct product for a valid ID.
+    """
+
+    resource = ProductsResource()
+    product_id = 1  # Assuming a product exists with ID 1
+    response = resource.get(product_id)
+
+    assert response.status_code == 200
+    assert response.json["product_id"] == product_id
+
+# Test updating a product
+def test_update_product():
+    """
+    Test that `update_product` updates the correct product for a valid ID.
+    """
+
+    resource = ProductsResource()
+    product_id = 1  # Assuming a product exists with ID 1
+    updated_data = {
+        "product_name": "Updated Test Product",
+        "quantity": 20,
+    }
+
+    response = resource.patch(product_id, json=updated_data)
+    data = response.json
+
+    assert response.status_code == 200
+    assert data["product_name"] == "Updated Test Product"
+    assert data["quantity"] == 20
+
+# Test deleting a product
+def test_delete_product():
+    """
+    Test that `delete_product` deletes the correct product for a valid ID.
+    """
+
+    resource = ProductsResource()
+    product_id = 1  # Assuming a product exists with ID 1
+    response = resource.delete(product_id)
+
+    assert response.status_code == 200
+    assert response.json["message"] == "Product deleted successfully"
