6110c7fc1dcf6537b800950a5645514db89bccf7
mark kibocha <kibochamark@gmail.com>
2023-12-11 10:54:47 +0300

    update on stores

--- api/models/sales.py
+++ api/models/sales.py
@@ -1,34 +1,19 @@
 from ..utils import db
-from .association import store_product_association
 
-class Store(db.Model):
-    __tablename__ = 'stores'
 
-    store_id = db.Column(db.Integer(), primary_key=True)
-    store_name = db.Column(db.String(255), nullable=True)
-    location = db.Column(db.String(255))
-    user_id = db.Column(db.Integer(), db.ForeignKey('users.user_id', ondelete='SET NULL'), nullable=True)
+class Sales(db.Model):
+    __tablename__ = 'sales'
 
-    # Relationship with the 'Products' table through the association table
-    products = db.relationship('Products', secondary=store_product_association, back_populates='stores')
-    supplier_id = db.Column(db.Integer(), db.ForeignKey('suppliers.supplier_id'), nullable=True)
-
-    # Relationship with the 'Sales' table
-    sales = db.relationship('Sales', backref='store', cascade='all, delete-orphan')
-
+    sale_id = db.Column(db.Integer(), primary_key=True)
+    product_id = db.Column(db.Integer(), db.ForeignKey('products.product_id'))
+    product_quantity = db.Column(db.Integer())
+    store_id = db.Column(db.Integer(), db.ForeignKey('stores.store_id'))
+    amount = db.Column(db.Integer())
+    
     def save(self):
         db.session.add(self)
         db.session.commit()
 
     def delete(self):
-        # Delete associated products
-        for product in self.products:
-            db.session.delete(product)
-
-        # Delete associated sales
-        for sale in self.sales:
-            db.session.delete(sale)
-
-        # Delete the store itself
         db.session.delete(self)
         db.session.commit()
--- api/models/stores.py
+++ api/models/stores.py
@@ -5,16 +5,16 @@ class Store(db.Model):
     __tablename__ = 'stores'
 
     store_id = db.Column(db.Integer(), primary_key=True)
-    store_name=db.Column(db.String(255), nullable=True)
+    store_name = db.Column(db.String(255), nullable=True)
     location = db.Column(db.String(255))
-    user_id = db.Column(db.Integer(),  db.ForeignKey('users.user_id', ondelete='SET NULL'), nullable=True)
-    
-    
-  # Relationship with the 'Products' table through the association table
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.user_id', ondelete='SET NULL'), nullable=True)
+
+    # Relationship with the 'Products' table through the association table
     products = db.relationship('Products', secondary=store_product_association, back_populates='stores')
-    
     supplier_id = db.Column(db.Integer(), db.ForeignKey('suppliers.supplier_id'), nullable=True)
 
+    # Relationship with the 'Sales' table
+    sales = db.relationship('Sales', backref='store', cascade='all, delete-orphan')
 
     def save(self):
         db.session.add(self)
@@ -25,9 +25,10 @@ class Store(db.Model):
         for product in self.products:
             db.session.delete(product)
 
+        # Delete associated sales
+        for sale in self.sales:
+            db.session.delete(sale)
+
         # Delete the store itself
         db.session.delete(self)
         db.session.commit()
-
-
-  
